7cbba3e6d2ee795a37e87df16696a0c72f0f117a
ignatius22 <igee54077@gmail.com>
2020-11-18 00:26:29 +0100

    add admin template

--- route/farmAdminRoute.js
+++ route/farmAdminRoute.js
@@ -1,261 +1,363 @@
-import express from 'express'
-const router = express.Router()
+import express from 'express';
+const router = express.Router();
 
-
-
-
-router.get('/badges', function(req, res) {
-    res.render('adminView/badges', {layout: 'farmView.handlebars'});
+router.get('/home', (req, res) => {
+  res.render('adminFarm/index', {
+    layout: 'admin'
+  });
 });
 
 
-router.get('/error-404', function (req, res) {
-  res.render('adminView/404', { layout: 'farmView.handlebars' });
+router.get('/accordion', (req, res) => {
+  res.render('adminFarm/accordion', {
+    layout: 'admin',
+  });
 });
 
-router.get('/app-tour', function (req, res) {
-  res.render('adminView/app-tour', { layout: 'farmView.handlebars' });
+router.get('/badges', (req, res) => {
+  res.render('adminFarm/badges', {
+    layout: 'admin',
+  });
 });
 
-router.get('/error-500', function (req, res) {
-  res.render('adminView/500', { layout: 'farmView.handlebars' });
-});
-
-
-router.get('/error-505', function (req, res) {
-  res.render('adminView/505', { layout: 'farmView.handlebars' });
-});
-
-router.get('/accordion', function (req, res) {
-  res.render('adminView/accordion', { layout: 'farmView.handlebars' });
-});
-
-router.get('/alert', function (req, res) {
-  res.render('adminView/alert', { layout: 'farmView.handlebars' });
-});
-
-router.get('/am-maps', function (req, res) {
-  res.render('adminView/am-maps', { layout: 'farmView.handlebars' });
-});
-
-router.get('/basic-table', function (req, res) {
-  res.render('adminView/basic-table', { layout: 'farmView.handlebars' });
-});
-
-router.get('/blank', function (req, res) {
-  res.render('adminView/blank', { layout: 'farmView.handlebars' });
-});
-
-router.get('/buttons', function (req, res) {
-  res.render('adminView/buttons', { layout: 'farmView.handlebars' });
-});
-
-router.get('/c3-chart', function (req, res) {
-  res.render('adminView/c3-chart', { layout: 'farmView.handlebars' });
-});
-
-router.get('/cards', function (req, res) {
-  res.render('adminView/cards', { layout: 'farmView.handlebars' });
-});
-
-router.get('/carousel', function (req, res) {
-  res.render('adminView/carousel', { layout: 'farmView.handlebars' });
-});
-
-router.get('/chart-js', function (req, res) {
-  res.render('adminView/chart-js', { layout: 'farmView.handlebars' });
-});
-
-router.get('/chartlist', function (req, res) {
-  res.render('adminView/chartlist', { layout: 'farmView.handlebars' });
-});
-
-router.get('/code-editor', function (req, res) {
-  res.render('adminView/code-editor', { layout: 'farmView.handlebars' });
-});
-
-router.get('/compose', function (req, res) {
-  res.render('adminView/compose', { layout: 'farmView.handlebars' });
-});
-
-router.get('/cropper', function (req, res) {
-  res.render('adminView/cropper', { layout: 'farmView.handlebars' });
-});
-
-router.get('/datatable', function (req, res) {
-  res.render('adminView/datatable', { layout: 'farmView.handlebars' });
-});
-
-router.get('/dropdown', function (req, res) {
-  res.render('adminView/dropdown', { layout: 'farmView.handlebars' });
-});
-
-router.get('/dropzone', function (req, res) {
-  res.render('adminView/dropzone', { layout: 'farmView.handlebars' });
-});
-
-router.get('/et-line', function (req, res) {
-  res.render('adminView/et-line', { layout: 'farmView.handlebars' });
-});
-
-router.get('/fontawesome', function (req, res) {
-  res.render('adminView/fontawesome', { layout: 'farmView.handlebars' });
-});
-
-router.get('/forgot-password', function (req, res) {
-  res.render('adminView/forgot-password', { layout: 'farmView.handlebars' });
-});
-
-router.get('/form-basic', function (req, res) {
-  res.render('adminView/form-basic', { layout: 'farmView.handlebars' });
-});
-
-router.get('/form-groups', function (req, res) {
-  res.render('adminView/form-groups', { layout: 'farmView.handlebars' });
-});
-
-router.get('/form-layouts', function (req, res) {
-  res.render('adminView/form-layouts', { layout: 'farmView.handlebars' });
+router.get('/login', (req, res) => {
+  res.render('adminFarm/login', {
+    layout: 'admin',
+  });
 });
 
 
-router.get('/form-validation', function (req, res) {
-  res.render('adminView/form-validation', { layout: 'farmView.handlebars' });
+router.get('/table', (req, res) => {
+  res.render('adminFarm/basic-table', {
+    layout: 'admin',
+  });
 });
 
-router.get('/full-calender', function (req, res) {
-  res.render('adminView/full-calender', { layout: 'farmView.handlebars' });
+router.get('/blank', (req, res) => {
+  res.render('adminFarm/blank', {
+    layout: 'admin',
+  });
 });
 
-router.get('/gallery', function (req, res) {
-  res.render('adminView/gallery', { layout: 'farmView.handlebars' });
+router.get('/alert', (req, res) => {
+  res.render('adminFarm/alert', {
+    layout: 'admin',
+  });
+});
+
+router.get('/am-maps', (req, res) => {
+  res.render('adminFarm/am-maps', {
+    layout: 'admin',
+  });
+});
+
+router.get('/app-tour', (req, res) => {
+  res.render('adminFarm/app-tour', {
+    layout: 'admin',
+  });
+});
+
+router.get('/buttons', (req, res) => {
+  res.render('adminFarm/buttons', {
+    layout: 'admin',
+  });
+});
+
+router.get('/c3-chart', (req, res) => {
+  res.render('adminFarm/c3-chart', {
+    layout: 'admin',
+  });
+});
+
+router.get('/cards', (req, res) => {
+  res.render('adminFarm/cards', {
+    layout: 'admin',
+  });
+});
+
+router.get('/carousel', (req, res) => {
+  res.render('adminFarm/carousel', {
+    layout: 'admin',
+  });
+});
+
+router.get('/chart-js', (req, res) => {
+  res.render('adminFarm/chart-js', {
+    layout: 'admin',
+  });
+});
+
+router.get('/chartlist', (req, res) => {
+  res.render('adminFarm/chartlist', {
+    layout: 'admin',
+  });
+});
+
+router.get('/code-editor', (req, res) => {
+  res.render('adminFarm/code-editor', {
+    layout: 'admin',
+  });
+});
+
+router.get('/compose', (req, res) => {
+  res.render('adminFarm/compose', {
+    layout: 'admin',
+  });
+});
+
+router.get('/cropper', (req, res) => {
+  res.render('adminFarm/cropper', {
+    layout: 'admin',
+  });
+});
+
+router.get('/datatable', (req, res) => {
+  res.render('adminFarm/datatable', {
+    layout: 'admin',
+  });
+});
+
+router.get('/dropdown', (req, res) => {
+  res.render('adminFarm/dropdown', {
+    layout: 'admin',
+  });
+});
+
+router.get('/dropzone', (req, res) => {
+  res.render('adminFarm/dropzone', {
+    layout: 'admin',
+  });
+});
+
+router.get('/et-line', (req, res) => {
+  res.render('adminFarm/et-line', {
+    layout: 'admin',
+  });
+});
+
+router.get('/font-awesome', (req, res) => {
+  res.render('adminFarm/font-awesome', {
+    layout: 'admin',
+  });
+});
+
+router.get('/forgot-password', (req, res) => {
+  res.render('adminFarm/forgot-password', {
+    layout: 'admin',
+  });
 });
 
 
-router.get('/google-maps', function (req, res) {
-  res.render('adminView/google-maps', { layout: 'farmView.handlebars' });
+router.get('/form-basic', (req, res) => {
+  res.render('adminFarm/form-basic', {
+    layout: 'admin',
+  });
 });
 
-router.get('/grid', function (req, res) {
-  res.render('adminView/grid', { layout: 'farmView.handlebars' });
+router.get('/form-groups', (req, res) => {
+  res.render('adminFarm/form-groups', {
+    layout: 'admin',
+  });
 });
 
-router.get('/inbox', function (req, res) {
-  res.render('adminView/inbox', { layout: 'farmView.handlebars' });
+router.get('/form-layouts', (req, res) => {
+  res.render('adminFarm/form-layouts', {
+    layout: 'admin',
+  });
 });
 
-
-router.get('/index2', function (req, res) {
-  res.render('adminView/index2', { layout: 'farmView.handlebars' });
+router.get('/form-validation', (req, res) => {
+  res.render('adminFarm/form-validation', {
+    layout: 'admin',
+  });
 });
 
-router.get('/index3', function (req, res) {
-  res.render('adminView/index3', { layout: 'farmView.handlebars' });
+router.get('/full-callender', (req, res) => {
+  res.render('adminFarm/full-calender', {
+    layout: 'admin',
+  });
 });
 
-router.get('/invoice', function (req, res) {
-  res.render('adminView/invoice', { layout: 'farmView.handlebars' });
+router.get('/gallery', (req, res) => {
+  res.render('adminFarm/gallery', {
+    layout: 'admin',
+  });
 });
 
-router.get('/ionicons', function (req, res) {
-  res.render('adminView/ionicons', { layout: 'farmView.handlebars' });
-});
-
-router.get('/js-grid', function (req, res) {
-  res.render('adminView/js-grid', { layout: 'farmView.handlebars' });
-});
-
-router.get('/loaders', function (req, res) {
-  res.render('adminView/loaders', { layout: 'farmView.handlebars' });
-});
-
-
-router.get('/ladda-button', function (req, res) {
-  res.render('adminView/ladda-button', { layout: 'farmView.handlebars' });
-});
-
-
-router.get('/list-group', function (req, res) {
-  res.render('adminView/list-group', { layout: 'farmView.handlebars' });
+router.get('/google-maps', (req, res) => {
+  res.render('adminFarm/google-maps', {
+    layout: 'admin',
+  });
 });
 
 
-router.get('/lock', function (req, res) {
-  res.render('adminView/lock', { layout: 'farmView.handlebars' });
+router.get('/grid', (req, res) => {
+  res.render('adminFarm/grid', {
+    layout: 'admin',
+  });
+});
+
+router.get('/inbox', (req, res) => {
+  res.render('adminFarm/inbox', {
+    layout: 'admin',
+  });
+});
+
+router.get('/index2', (req, res) => {
+  res.render('adminFarm/index2', {
+    layout: 'admin',
+  });
+});
+
+router.get('/index3', (req, res) => {
+  res.render('adminFarm/index3', {
+    layout: 'admin',
+  });
+});
+
+router.get('/ionicons', (req, res) => {
+  res.render('adminFarm/ionicons', {
+    layout: 'admin',
+  });
+});
+
+router.get('/js-grid', (req, res) => {
+  res.render('adminFarm/js-grid', {
+    layout: 'admin',
+  });
+});
+
+router.get('/ladda-button', (req, res) => {
+  res.render('adminFarm/ladda-button', {
+    layout: 'admin',
+  });
+});
+
+router.get('/list-group', (req, res) => {
+  res.render('adminFarm/list-group', {
+    layout: 'admin',
+  });
+});
+
+router.get('/loaders', (req, res) => {
+  res.render('adminFarm/loaders', {
+    layout: 'admin',
+  });
+});
+
+router.get('/lock', (req, res) => {
+  res.render('adminFarm/lock', {
+    layout: 'admin',
+  });
+});
+
+router.get('/modals', (req, res) => {
+  res.render('adminFarm/modals', {
+    layout: 'admin',
+  });
+});
+
+router.get('/morris-chart', (req, res) => {
+  res.render('adminFarm/morris-charts', {
+    layout: 'admin',
+  });
+});
+
+router.get('/pagination', (req, res) => {
+  res.render('adminFarm/pagination', {
+    layout: 'admin',
+  });
+});
+
+router.get('/pagination', (req, res) => {
+  res.render('adminFarm/pagination', {
+    layout: 'admin',
+  });
+});
+
+router.get('/popover', (req, res) => {
+  res.render('adminFarm/popover', {
+    layout: 'admin',
+  });
+});
+
+router.get('/pricing', (req, res) => {
+  res.render('adminFarm/pricing', {
+    layout: 'admin',
+  });
+});
+router.get('/profile', (req, res) => {
+  res.render('adminFarm/profile', {
+    layout: 'admin',
+  });
+});
+
+router.get('/progressbar', (req, res) => {
+  res.render('adminFarm/progressbar', {
+    layout: 'admin',
+  });
+});
+
+router.get('/read-mail', (req, res) => {
+  res.render('adminFarm/read-mail', {
+    layout: 'admin',
+  });
+});
+
+router.get('/register', (req, res) => {
+  res.render('adminFarm/register', {
+    layout: 'admin',
+  });
 });
 
 
-router.get('/login', function (req, res) {
-  res.render('adminView/login', { layout: 'farmView.handlebars' });
+router.get('/reset-password', (req, res) => {
+  res.render('adminFarm/reset-password', {
+    layout: 'admin',
+  });
+});
+
+router.get('/sweet-notification', (req, res) => {
+  res.render('adminFarm/sweet-notification', {
+    layout: 'admin',
+  });
+});
+
+router.get('/tabs', (req, res) => {
+  res.render('adminFarm/tabs', {
+    layout: 'admin',
+  });
+});
+
+router.get('/text-editor', (req, res) => {
+  res.render('adminFarm/text-editor', {
+    layout: 'admin',
+  });
+});
+
+router.get('/themify', (req, res) => {
+  res.render('adminFarm/themify', {
+    layout: 'admin',
+  });
+});
+
+router.get('/timeline', (req, res) => {
+  res.render('adminFarm/timeline', {
+    layout: 'admin',
+  });
 });
 
 
-router.get('/modals', function (req, res) {
-  res.render('adminView/modals', { layout: 'farmView.handlebars' });
+router.get('/toastr', (req, res) => {
+  res.render('adminFarm/toastr', {
+    layout: 'admin',
+  });
 });
 
-router.get('/morris-charts', function (req, res) {
-  res.render('adminView/morris-charts', { layout: 'farmView.handlebars' });
+router.get('/typography', (req, res) => {
+  res.render('adminFarm/typography', {
+    layout: 'admin',
+  });
 });
-
-
-router.get('/pagination', function (req, res) {
-  res.render('adminView/pagination', { layout: 'farmView.handlebars' });
-});
-
-router.get('/pricing', function (req, res) {
-  res.render('adminView/pricing', { layout: 'farmView.handlebars' });
-});
-
-router.get('/profile', function (req, res) {
-  res.render('adminView/profile', { layout: 'farmView.handlebars' });
-});
-
-
-router.get('/progressbar', function (req, res) {
-  res.render('adminView/progressbar', { layout: 'farmView.handlebars' });
-});
-
-
-router.get('/read-mail', function (req, res) {
-  res.render('adminView/read-mail', { layout: 'farmView.handlebars' });
-});
-
-
-//got some issues with this line
-
-router.get('/registeration', function (req, res) {
-  res.render('adminView/register', { layout: 'farmView.handlebars' });
-});
-
-router.get('/reset-password', function (req, res) {
-  res.render('adminView/reset-password', { layout: 'farmView.handlebars' });
-});
-
-
-router.get('/sweet-notification', function (req, res) {
-  res.render('adminView/sweet-notification', { layout: 'farmView.handlebars' });
-});
-
-router.get('/tabs', function (req, res) {
-  res.render('adminView/tabs', { layout: 'farmView.handlebars' });
-});
-
-router.get('/themify', function (req, res) {
-  res.render('adminView/themify', { layout: 'farmView.handlebars' });
-});
-
-router.get('/timeline', function (req, res) {
-  res.render('adminView/timeline', { layout: 'farmView.handlebars' });
-});
-
-router.get('/toastr', function (req, res) {
-  res.render('adminView/toastr', { layout: 'farmView.handlebars' });
-});
-
-router.get('/typography', function (req, res) {
-  res.render('adminView/typography', { layout: 'farmView.handlebars' });
-});
-
 
 export default router;
--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 import express from 'express';
-import exphbs  from 'express-handlebars';
-import farmRouter from './route/farmRouter'
-import farmAdminRouter from './route/farmAdminRoute'
-const app = express()
+import exphbs from 'express-handlebars';
+import farmRouter from './route/farmRouter';
+import adminFarmRouter from './route/farmAdminRoute'
+const app = express();
 
 const port = 3000;
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-app.use(express.static('public/'));
+app.use(express.static('public'));
+
+app.use('/', farmRouter);
+app.use('/admin', adminFarmRouter);
 
 
-app.use('/', farmRouter)
-app.use('/admin', farmAdminRouter)
-
-
-app.listen(port, ()=> {
-  console.log('app running on ' + port)
+app.listen(port, () => {
+  console.log('app running on ' + port);
 });
